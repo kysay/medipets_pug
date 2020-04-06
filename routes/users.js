b419e83fe75edbfc79ddb0c237afce4882c87ba2
kys_mac <tlfldntm12@naver.com>
2020-04-06 13:10:33 +0900

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+console.log('1')
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('users', { title: 'users' });
+  // res.send('respond with a resource');
+  res.send('123');
+});
+
+module.exports = router;
