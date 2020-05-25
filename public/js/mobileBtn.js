abdd61225ec4aff8ab77644091a5d60a69b1aa13
kys <tlfldntm12@naver.com>
2020-05-26 01:36:03 +0900

    모바일 버튼

--- public/js/mobileBtn.js
+++ public/js/mobileBtn.js
@@ -1,10 +1,7 @@
-console.log('click');
-
-
 $(document).ready(() => {
-    $('#mobileNav, .mobile_wrap').on('click', ()=>{
-        console.log('click')
-        $('#mobileNav').toggleClass( 'active' );
+    $('#mobileNav').on('click', ()=>{
+        console.log('click');
+        $('.mobile_wrap, #mobileNav').toggleClass( 'active' );
     });
 
     console.log($('.location_wrap').text().toUpperCase());
