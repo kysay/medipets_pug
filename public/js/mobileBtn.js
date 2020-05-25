832ae630b68b71362c2e268b4f42efc3b6f8b092
kys <tlfldntm12@naver.com>
2020-05-26 01:47:11 +0900

    footer 수정

--- public/js/mobileBtn.js
+++ public/js/mobileBtn.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
     $('#mobileNav').on('click', ()=>{
         console.log('click');
         $('.mobile_wrap, #mobileNav').toggleClass( 'active' );
+        $('html').toggleClass( 'overflow_h' );
     });
 
     console.log($('.location_wrap').text().toUpperCase());
