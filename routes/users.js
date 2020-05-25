b1549d343136a8e1548c6714df0db8fcba1e1d88
kys <tlfldntm12@naver.com>
2020-05-26 01:54:17 +0900

    footer 수정

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 // var app = require('../app');
 
-
-console.log('users page');
-/* GET users listing. */
-
 router.get('/', function(req, res, next) {
   res.render('users', { title: 'users' });
   // res.send('respond with a resource');
